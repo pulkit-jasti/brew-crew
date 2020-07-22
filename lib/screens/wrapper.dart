56d3d2588d0d42595a49b808e5951f7b24de24b2
pulkit-jasti <31801730+pulkit-jasti@users.noreply.github.com>
2020-07-22 21:32:37 +0530

    rebuild from ground up

---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+import 'authenticate/authenticate.dart';
+import 'home/home.dart';
+
+class wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return home();
+  }
+}
