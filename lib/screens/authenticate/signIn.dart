0bfc64dcc8868dd63ca0f1c1e38655c2e3bf7c5f
pulkit-jasti <31801730+pulkit-jasti@users.noreply.github.com>
2020-07-23 16:41:20 +0530

    sign in annonymously update

--- lib/screens/authenticate/signIn.dart
+++ lib/screens/authenticate/signIn.dart
@@ -26,7 +26,7 @@ class _signInState extends State<signIn> {
               print('error signing in');
             } else {
               print('signed in');
-              print(result);
+              print(result.userID);
             }
           },
         ),
--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,14 +1,19 @@
+import 'package:brew_crew/models/user.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class authService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  User _userFromFirebaseUser(FirebaseUser user) {
+    return user != null ? User(userID: user.uid) : null;
+  }
 
   //sign-in anyms
   Future signInAnyms() async {
     try {
       AuthResult result = await _auth.signInAnonymously();
       FirebaseUser user = result.user;
-      return user;
+      return _userFromFirebaseUser(user);
     } catch (e) {
       print(e.toString());
       return null;
